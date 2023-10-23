55cf05ec3fa12eda5eb8822e976982239c0608db
rafwit <rwitczak89@gmail.com>
2023-10-23 18:06:48 +0200

    enable receipe app

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -30,4 +31,6 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
+
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,12 @@
+"""Test for the models
+"""
+
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
 
 class ModelTests(TestCase):
 
@@ -18,7 +25,8 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         sample_emails = [
-            ["test1@EXAMPLE.com", "test1@example.com"], ["Test2@example.com", "Test2@example.com"], ["TEST3@EXAMPLE.COM", "TEST3@example.com"], ["test4@example.com", "test4@example.com"]
+            ["test1@EXAMPLE.com", "test1@example.com"], ["Test2@example.com", "Test2@example.com"], [
+                "TEST3@EXAMPLE.COM", "TEST3@example.com"], ["test4@example.com", "test4@example.com"]
         ]
 
         for email, expected in sample_emails:
@@ -38,3 +46,17 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a new recipe"""
+        user = get_user_model().objects.create_user('test@example.com', 'test123')
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Simple recipe name",
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
