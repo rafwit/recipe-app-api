100a96d2c67f713991f37a014dc762b70623a34f
rafwit <rwitczak89@gmail.com>
2023-10-23 15:53:59 +0200

    add swagger

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,3 +23,22 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+
+    def test_edit_user_page(self):
+        """Test edit page works
+        """
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test create user page works
+        """
+
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
